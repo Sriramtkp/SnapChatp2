5f2bf15f070956d90f5fdac1062d3e40e44d2706
Sriram Rajendran <sriramrajendrantkp@yahoo.in>
2016-10-12 11:42:56 +0530

    Initial Commit

---
+++ SnapChatp2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SnapChatp2
+//
+//  Created by Sriram Rajendran on 12-10-16.
+//  Copyright © 2016 Sriram Rajendran. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
